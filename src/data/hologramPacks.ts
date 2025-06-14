3b45f23c7b7f2e436d5e751c7229de45e8e0a077
Guihjzzz <junindacosta064@gmail.com>
2025-06-14 11:32:28 -0300

    Update hologramPacks.ts

--- src/data/hologramPacks.ts
+++ src/data/hologramPacks.ts
@@ -3,7 +3,7 @@ import { LinkItem } from "@/hooks/useLinks";
 
 export const hologramPacksData: LinkItem[] = [
   {
-    id: '113',
+    id: 'holo-113',
     title: 'Pack Holograma Medieval',
     description: 'Pacote completo de hologramas medievais',
     url: 'https://terabox.app/s/1Bxwy-i_Spo4gqwhL4n1wyw',
@@ -11,7 +11,7 @@ export const hologramPacksData: LinkItem[] = [
     imageUrl: 'https://bio.linkcdn.cc/upload/2025051512/1747316933748.jpeg'
   },
   {
-    id: '114',
+    id: 'holo-114',
     title: 'Pack Holograma centro de trocas',
     description: 'Pacote de hologramas para centros de trocas',
     url: 'https://terabox.app/s/1euM7GSb5FNVMQzn1sdX5Cg',
@@ -19,7 +19,7 @@ export const hologramPacksData: LinkItem[] = [
     imageUrl: 'https://bio.linkcdn.cc/upload/2025053022/1748645407245.png'
   },
   {
-    id: '115',
+    id: 'holo-115',
     title: 'Pack Holograma Casa Pequenas',
     description: 'Pacote de hologramas para casas pequenas',
     url: 'https://terabox.app/s/1QgLHmH4IKyr_XMkCe_M21g',
